5cd95b0e1769566d33feaa8d74f8d09ae9137447
Anton Mikhailov <tony.mikhailov@gmail.com>
2025-10-04 15:55:11 +0300

    * тест для генерации частоты с целочисленным делителем

--- ztester.c
+++ ztester.c
@@ -6,19 +6,18 @@
 
 #define OUTPUT_PIN 14
 
-#define PICO_CLOCK_ADJUST_PERI_CLOCK_WITH_SYS_CLOCK 1
-
 int main() {
     PIO pio = pio0;
     uint sm = pio_claim_unused_sm(pio, true);
     uint offset = pio_add_program(pio, &squarewave_program);
 
-    set_sys_clock_khz(112000, true);
-    // Пример для частоты 56 МГц (N=16)
-//    float desired_freq = 3.5e6 * 16; // 56 МГц
+//    set_sys_clock_khz(112000, true);
+
+    //    float desired_freq = 3.5e6 * 16; // 56 МГц
 //    float desired_freq = 3.5e6;
 //    float desired_freq = 14e6;
-    float desired_freq = 56e6;
+///    float desired_freq = 56e6;
+    float desired_freq = 31.25e6;
     float div = (float)clock_get_hz(clk_sys) / (2 * desired_freq); // 125,000,000 / (2 * 56,000,000) ≈ 1.116
 
     squarewave_program_init(pio, sm, offset, OUTPUT_PIN, div);
